dfcac7da9587d928ab3a239b83708ba693316395
Bhargav Maniar <bhargav.mahendrabhai2013@vit.ac.in>
2019-04-06 13:47:22 +0530

    adding fake data generation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-
+import { fetchUsers } from "./services/fakeDataGenerator.service";
 import "./styles.css";
 
-function App() {
+const App = () => {
+  const [initialized, setInitialized] = useState(false);
+  const [userData, setUserData] = useState([]);
+  useEffect(() => {
+    if (!initialized) {
+      fetchUsers().then(users => {
+        setUserData(users);
+      });
+      setInitialized(true);
+    }
+  });
+  const renderUserData = () => {
+    return userData.slice(0, 20).map((user, index) => {
+      return (
+        <tr key={index}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>
+            <h5>{user.email}</h5>
+          </td>
+        </tr>
+      );
+    });
+  };
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="row margin-top">
+        <div className="col-md-12">
+          <table>
+            <tbody>{renderUserData()}</tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
